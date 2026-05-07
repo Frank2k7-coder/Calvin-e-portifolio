1306b63f103b493e600401c6a5dc8b4c54998a12
Byiringiro Frank <frankbyiringiro2@gmail.com>
2026-05-07 18:51:41 +0200

    Update contact.tsx

--- components/contact.tsx
+++ components/contact.tsx
@@ -39,8 +39,7 @@ export default function Contact() {
 
   const socials = [
     { icon: Instagram, label: 'Instagram', href: 'https://www.instagram.com/n_calvin.maker' },
-    { icon: Facebook, label: 'Facebook', href: '#' },
-    { icon: Linkedin, label: 'LinkedIn', href: 'https://www.linkedin.com/in/nehemie-ntihebuwayo-698b0a37b/' },
+    { icon: Linkedin, label: 'LinkedIn', href: 'https://www.instagram.com/nehemy.log/' },
   ]
 
   return (
